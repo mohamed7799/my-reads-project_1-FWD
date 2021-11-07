d38c71ee5586080cf6871cc187bb01036cf90728
mohmed <mohmedamged9@gmail.com>
2021-11-07 22:12:03 +0200

    feat:implement the changing shelfs functionanlity on the main page

--- src/components/book.js
+++ src/components/book.js
@@ -1,6 +1,42 @@
+import { useEffect, useState } from "react";
+import { update, getAll } from "../api/BooksApi";
+import { useContext } from "react";
+import { MY_BOOKS_CONTEXT } from "../contexts/my_books_context";
 const Book = ({ book }) => {
+  const { setMyBooks } = useContext(MY_BOOKS_CONTEXT);
+
+  const [selectedShelf, setSelectedShelf] = useState(book.shelf);
+
+  const changeShelf = async () => {
+    try {
+      const isUpdated = await update(book, selectedShelf);
+      if (isUpdated) {
+        const res = await getAll();
+        setMyBooks(res);
+      }
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  useEffect(() => {
+    console.log(selectedShelf);
+    changeShelf();
+  }, [selectedShelf]);
+
   return (
     <li className="p-2 text-center">
+      <select
+        value={selectedShelf}
+        onChange={(e) => setSelectedShelf(e.target.value)}
+        className="mb-3"
+        name="shelf"
+      >
+        <option value="currentlyReading">Currently Reading</option>
+        <option value="wantToRead">Want To Read</option>
+        <option value="read">Read</option>
+        <option value="none">None</option>
+      </select>
       <img
         className="mx-auto"
         src={book.imageLinks.thumbnail}
